8b34322e21d3969a79a1d6f0c7d7ade532449254
Michaelsladkov <msladkov2002@gmail.com>
2023-03-23 23:46:27 +0300

    userspace error handling reworked

--- user/user.c
+++ user/user.c
@@ -29,17 +29,18 @@ int main(int argc, char **argv)
     if (argc == 1)
     {
         printf("Command required!\n");
+        fclose(f);
         return -1;
     }
 
     char *command = argv[1];
     uint8_t val;
+    bool val_is_good = true;
     if (strcmp("read", command) == 0)
     {
         if (fread(&val, sizeof(uint8_t), 1, f) < 1)
         {
             printf("Error while read\n");
-            return -1;
         }
         else
         {
@@ -48,32 +49,37 @@ int main(int argc, char **argv)
     }
     else if (strcmp("write", command) == 0)
     {
-        if (argc == 2)
+        if (argc <= 2)
         {
             printf("New speed val required!\n");
-            return -1;
-        }
-        if (!check_if_num(argv[2]))
-        {
-            printf("New speed val should be numeric!\n");
-        }
-        val = atoi(argv[2]);
-        if (val < 0 || val > 255)
-        {
-            printf("New speed val should be in [0; 255]!\n");
-        }
-        if (fwrite(&val, 1, 1, f) < 1)
-        {
-            printf("Error while write");
-            return -1;
         }
         else
         {
-            printf("Speed changed to: %s", &val);
+            if (check_if_num(argv[2]))
+            {
+                val = atoi(argv[2]);
+                if (val < 0 || val > 255)
+                {
+                    printf("New speed val should be in [0; 255]!\n");
+                }
+                else if (fwrite(&val, 1, 1, f) < 1)
+                {
+                    printf("Error while write");
+                }
+                else
+                {
+                    printf("Speed changed to: %s", &val);
+                }
+            }
+            else
+            {
+                printf("New speed val should be numeric!\n");
+            }
         }
-    } else {
+    } else 
+    {
         printf("Command unknown!");
-        return -1;
     }
+    fclose(f);
     return 0;
 }
