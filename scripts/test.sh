80ca95f10ac37de41f4aa594c63093e2bd84ee5b
Michaelsladkov <msladkov2002@gmail.com>
2023-03-28 11:21:04 +0300

    extract number from user out

--- scripts/test.sh
+++ scripts/test.sh
@@ -11,12 +11,13 @@ do
     setting=$((p+i*25))
     echo Set speed to ${setting}
     ${REPO_DIR}/user/build/user write ${setting}
-    fan_speed=`${REPO_DIR}/user/build/user read`
+    fan_speed_msg=`${REPO_DIR}/user/build/user read`
+    fan_speed=${fan_speed_msg##* }
     if [[ "$fan_speed" -eq "$setting" ]]
     then
         echo Speed setting correctly applied
     else
-        echo Speed setting isn\'t applied. Fan speed is ${fan_speed}
+        echo Speed setting isn\'t applied. ${fan_speed_msg}
     fi
     sleep 3
 done
